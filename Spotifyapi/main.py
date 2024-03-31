7a73a17a4623717fb0e9ca9cbef2cf1d7fb6fd8d
KH104 <91711387+KH104@users.noreply.github.com>
2024-04-01 01:14:13 +0530

    Update main.py

--- Spotifyapi/main.py
+++ Spotifyapi/main.py
@@ -9,7 +9,7 @@ load_dotenv()
 client_id=os.getenv("CLIENT_ID")
 client_secret=os.getenv("CLIENT_SECRET")
 
-# print(client_id,client_secret)
+# Getting spotify API token to access the web API features
 
 def get_token():
     auth_string= client_id + ":" + client_secret
@@ -20,15 +20,20 @@ def get_token():
     headers={
         "Authorization": "Basic " + auth_byte64,
         "Content-Type": "application/x-www-form-urlencoded"
-
     }
+    
     data = {"grant_type": "client_credentials"}
     result = post(url, headers=headers, data=data)
     json_result= json.loads(result.content)
     token=json_result["access_token"]
     return token
+    
+    # Defining a function to create authorization headers using the access token
+
 def auth_get_header(token):
     return{"Authorization": "Bearer " + token}
+    
+# Searching for Artist ID 
 
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
@@ -45,6 +50,8 @@ def search_for_artist(token, artist_name):
     
     return json_result[0]
 
+# Pulling top songs of artist using artist ID
+
 def get_songs(token,artist_id):
     url=f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
     headers= auth_get_header(token)
@@ -52,15 +59,15 @@ def get_songs(token,artist_id):
     json_result= json.loads(result.content)["tracks"]
     return json_result
 
+# Asking for artist name
+
 artist = input("Artist name?: ")
 token = get_token()
 result = search_for_artist(token, artist)
 artist_id=result["id"]
 songs=get_songs(token, artist_id)
 
+# Displaying top songs using Index
+
 for idx, song in enumerate(songs):
     print(f"{idx + 1}. {song['name']}")
-
-
-
-
